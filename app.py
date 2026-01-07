e3ee82b6033164da4e0382900bbae92fdf8f359c
Serena Beelen <serenabeelen@outlook.com>
2026-01-07 14:55:06 +0100

    Nieuwe html files aangemaakt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -13,6 +14,18 @@ def contact():
 @app.route("/experiments")
 def experiments():
   return render_template("experiments.html")
+
+@app.route("/cosmetics")
+def cosmetics():
+  return render_template("cosmetics.html")
+
+@app.route("/food")
+def food():
+  return render_template("food.html")
+
+@app.route("/fun")
+def fun():
+  return render_template("fun.html")
 
 if __name__=="__main__":
   app.run(debug=True)
